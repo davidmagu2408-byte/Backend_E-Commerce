9f7ca4ecc254096ecff976bb6b687f98585af06f
davidmagu2408-byte <davidmagu2408@gmail.com>
2026-03-30 00:21:32 +0700

    update new api

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -4,10 +4,7 @@ const verifyToken = (req, res, next) => {
     const token = req.header("accessToken") || req.header("Authorization")?.replace("Bearer ", "");
     if (!token) return res.status(401).send("Access Denied");
     try {
-        const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-            if (err) return res.status(400).send("Invalid Token");
-            return user;
-        });
+        const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
         req.user = verified;
         next();
     } catch (err) {
