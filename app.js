c0e826ba03ceaa034793ade954dd3e705c9f6dc6
David Magu <davidmagu2408@gmail.com>
2026-09-05 07:35:33 +0700

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv/config");
+require("dotenv").config();
 const dns = require("node:dns/promises");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const cookieParser = require("cookie-parser");
